985907ce1720f60c86dca3710d11691de8f5963d
Ivan I. Atanasov <i.atanasov77@gmail.com>
2024-12-03 18:26:23 +0000

    Copy Types Definitions into dist folder at Build.

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,15 @@
 import typescript from '@rollup/plugin-typescript';
+import copy from 'rollup-plugin-copy'
 
 const typescriptOptions = {
     exclude: ["tests/**/*"],
     compilerOptions: { declaration: false },
+};
+
+const copyOptions = {
+    targets: [
+        { src: 'src/types/*', dest: 'dist/esm/types' }
+    ]
 };
 
 const data = [
@@ -10,7 +17,7 @@ const data = [
         external: ["typeorm", "sinon"],
         input: "src/index.ts",
         output: { file: "dist/esm/index.js", format: "esm" },
-        plugins: [typescript( typescriptOptions )],
+        plugins: [typescript( typescriptOptions ), copy( copyOptions )],
     },
 //     {
 //         external: ["typeorm", "sinon"],
